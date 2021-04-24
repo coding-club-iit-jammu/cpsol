e6dc67a577ec63a50ae93d1dd16d33e4978662e1
PositronPiercer <2017UCS0111@iitjammu.ac.in>
2021-04-24 14:02:32 +0530

    added elastic search

--- models/problem.js
+++ models/problem.js
@@ -1,34 +1,40 @@
 const mongoose = require('mongoose')
+const mongoostatic = require('mongoosastic')
 const schema = mongoose.Schema
 const userSchema = require('./user')
 const problemSchema = new schema({
     title : {
         type : String,
-        required : true
+        required : true,
+        es_indexed: true
     },
     link : {
         type : String,
-        required : false
+        required : false,
+        es_indexed: false
     },
     md : {
         type : String,
-        required : false
+        required : false,
+        es_indexed: false
     },
     video_link : {
         type : String,
-        required : true
+        required : true,
+        es_indexed: false
     },
     writeup_md : {
         type : String,
-        required : false
+        required : false,
+        es_indexed: false
     },
     uploaded_by : {
         type : userSchema,
-        required : true
+        required : true,
+        es_indexed: false
     }
 })
-problemSchema.index({
-    title : 'text'
-})
-const Problem = mongoose.model('Problem', problemSchema)
+problemSchema.plugin(mongoostatic)
+
+const Problem = mongoose.model('Problem', problemSchema, 'problems')
 module.exports = Problem
